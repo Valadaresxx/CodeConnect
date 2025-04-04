002f0097bee3d845f634152d896f3b6fb0eb1785
Gabriel Valadares <silva44gabriel@gmail.com>
2025-04-04 16:02:11 -0300

    loadingPhotoOk

--- script.js
+++ script.js
@@ -3,16 +3,37 @@ const inputUpload = document.getElementById("image-upload");
 
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
+})
 
-    inputUpload.addEventListener("change", function(e){
-        var file = e.target.files[0];
-    })
-    if(!file.type.match("image/png") && !file.type.match("image/jpeg")){
-        alert("Por favor, selecione uma outra image! png ou jpeg.");
-        return;
-    }
-    if(!file.size > 2 * 1024 *1024 ){
-        alert("Imagem grande demais! minimo de 2MB.");
-        return;
+function readImage(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url:leitor.result, nome:arquivo.name })
+        }
+
+        leitor.onerror = ()  => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+
+        leitor.readAsDataURL(arquivo);
+    } )
+
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeImg = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) =>{
+    const arquivo = evento.target.files[0];
+    if(arquivo){
+        try{
+            
+            const conteudoArquivo = await readImage(arquivo);
+            imagemPrincipal.src = conteudoArquivo.url;
+            nomeImg.textContent = conteudoArquivo.nome;
+        }catch(erro){
+            console.error("Erro na leitura do arquivo!")
+        }
     }
 })
