ab2ae3e80f4d3771011ad9f8eaa467685bc91171
Gabriel Valadares <silva44gabriel@gmail.com>
2025-04-07 16:34:37 -0300

    Format

--- script.js
+++ script.js
@@ -1,64 +1,60 @@
 const uploadBtn = document.getElementById("upload-btn");
-const inputUpload = document.getElementById("image-upload"); 
+const inputUpload = document.getElementById("image-upload");
 
 uploadBtn.addEventListener("click", () => {
-    inputUpload.click();
-})
+  inputUpload.click();
+});
 
 function readImage(arquivo) {
-    return new Promise((resolve, reject) => {
-        const leitor = new FileReader();
-        leitor.onload = () => {
-            resolve({ url:leitor.result, nome:arquivo.name })
-        }
+  return new Promise((resolve, reject) => {
+    const leitor = new FileReader();
+    leitor.onload = () => {
+      resolve({ url: leitor.result, nome: arquivo.name });
+    };
 
-        leitor.onerror = ()  => {
-            reject(`Erro na leitura do arquivo ${arquivo.name}`)
-        }
+    leitor.onerror = () => {
+      reject(`Erro na leitura do arquivo ${arquivo.name}`);
+    };
 
-        leitor.readAsDataURL(arquivo);
-    } )
-
+    leitor.readAsDataURL(arquivo);
+  });
 }
 
 const imagemPrincipal = document.querySelector(".main-imagem");
 const nomeImg = document.querySelector(".container-imagem-nome p");
 
-inputUpload.addEventListener("change", async (evento) =>{
-    const arquivo = evento.target.files[0];
-    if(arquivo){
-        try{
-            
-            const conteudoArquivo = await readImage(arquivo);
-            imagemPrincipal.src = conteudoArquivo.url;
-            nomeImg.textContent = conteudoArquivo.nome;
-        }catch(erro){
-            console.error("Erro na leitura do arquivo!")
-        }
+inputUpload.addEventListener("change", async (evento) => {
+  const arquivo = evento.target.files[0];
+  if (arquivo) {
+    try {
+      const conteudoArquivo = await readImage(arquivo);
+      imagemPrincipal.src = conteudoArquivo.url;
+      nomeImg.textContent = conteudoArquivo.nome;
+    } catch (erro) {
+      console.error("Erro na leitura do arquivo!");
     }
+  }
 });
 
 const inputCategorias = document.getElementById("categorias");
 const liTags = document.getElementById("lista-tags");
 
-inputCategorias.addEventListener("keypress", (evento) => { 
-    if(evento.key === "Enter"){
-        evento.preventDefault();
-        const tags = inputCategorias.value.trim();//remove espaço em branco .trim
-        if(tags !== ""){
-            const newTags = document.createElement("li");
-            newTags.innerHTML = `<p>${tags}</p> <img src="img/close-black.svg" class="remove-tag">`;
-            liTags.appendChild(newTags);
-            inputCategorias.value="";
-            
-        }
+inputCategorias.addEventListener("keypress", (evento) => {
+  if (evento.key === "Enter") {
+    evento.preventDefault();
+    const tags = inputCategorias.value.trim(); //remove espaço em branco .trim
+    if (tags !== "") {
+      const newTags = document.createElement("li");
+      newTags.innerHTML = `<p>${tags}</p> <img src="img/close-black.svg" class="remove-tag">`;
+      liTags.appendChild(newTags);
+      inputCategorias.value = "";
     }
+  }
 });
 
 liTags.addEventListener("click", (evento) => {
-    if (evento.target.classList.contains("remove-tag")){
-        const retiraTag = evento.target.parentElement;
-        liTags.removeChild(retiraTag);
-
-    }
-})
+  if (evento.target.classList.contains("remove-tag")) {
+    const retiraTag = evento.target.parentElement;
+    liTags.removeChild(retiraTag);
+  }
+});
